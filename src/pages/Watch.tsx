aa78ca0c6252b13c0e4515a64bacdd9b179816ee
muharrem gem <muharremgem28@gmail.com>
2022-12-08 15:00:57 +0300

    watch

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppSelector } from "../store/hooks";
 
 export default function Watch() {
-  return (
-    <div>Watch</div>
-  )
+  const [showMoreStatus, setShowMoreStatus] = useState<boolean>(false);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const currentPlaying = useAppSelector(
+    (state) => state.youtubeApp.currentPlaying
+  );
+  const recommendedVideos = useAppSelector(
+    (state) => state.youtubeApp.recommendedVideos
+  );
+  useEffect(() => {
+    if (id) {
+      dispatch(getVideoDetails(id));
+      setShowMoreStatus(false);
+    } else {
+      navigate("/");
+    }
+  }, [id, navigate, dispatch]);
+
+  useEffect(() => {
+    if (currentPlaying && id) dispatch(getRecommendedVideos(id));
+  }, [currentPlaying, dispatch, id]);
+
+  return <div>Watch</div>;
 }
