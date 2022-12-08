f2a5c31c1bec6fd0ccf2b2e11cda63e6349a40e7
muharrem gem <muharremgem28@gmail.com>
2022-12-08 11:05:56 +0300

    Card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useDispatch } from "react-redux";
+import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+import { HomePageVideos } from "../Types";
 
 export default function Home() {
   const dispatch = useAppDispatch();
@@ -20,6 +24,23 @@ export default function Home() {
       </div>
       <div className="flex" style={{ height: "92.5vh" }}>
         <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={650}
+          >
+            <div className="grid gap-y-14 gap-x-8 grid-cols-4 p-8">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
       </div>
     </div>
   );
