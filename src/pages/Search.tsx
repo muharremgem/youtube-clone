7c7b5d573de2712001e8f1ae726e6b208edea7b4
muharrem gem <muharremgem28@gmail.com>
2022-12-08 12:56:58 +0300

    searchTerm

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
+import { clearVideos } from "../store";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import { HomePageVideos } from "../Types";
 
 export default function Search() {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearVideos());
+      if (searchTerm === "") navigate("/");
+      else {
+        dispatch(getSearchPageVideos(false));
+      }
+    };
+  }, [dispatch, navigate, searchTerm]);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+  }, [dispatch]);
+
   return (
-    <div>Search</div>
-  )
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={600}
+          >
+            <div className="grid gap-y-14 gap-x-8 grid-cols-4 p-8">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
 }
